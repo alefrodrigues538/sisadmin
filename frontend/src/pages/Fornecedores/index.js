0e31bb3564d1308a321de6f9954290bc61117bb2
alefrodrigues538 <alefrodrigues538@gmail.com>
2020-11-23 15:42:17 -0300

    v0.01.4

--- frontend/src/pages/Fornecedores/index.js
+++ frontend/src/pages/Fornecedores/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import "./styles.css"
 import {
     Container, Row, Col,
-
+    Form, Button, Card, Table
 } from "react-bootstrap"
 
 export default function Fornecedores() {
@@ -11,6 +11,48 @@ export default function Fornecedores() {
             <Row>
                 <Col>
                     <h3>Fornecedores</h3>
+                </Col>
+            </Row>
+            <Row>
+                <Card>
+                    <Form inline>
+                        <Form.Row>
+                            <Form.Group controlId="formName">
+                                <Form.Label>Nome:</Form.Label>
+                                <Form.Control type="Text" placeholder="Digite um nome"></Form.Control>
+                            </Form.Group>
+                            <Form.Group controlId="formCnpj" >
+                                <Form.Label>CNPJ:</Form.Label>
+                                <Form.Control type="Text" width="100px" maxLength={18} placeholder="00.000.000/0001-00"></Form.Control>
+                            </Form.Group>
+                        </Form.Row>
+                        <Button variant="primary" type="submit">Buscar</Button>
+                        <Button variant="dark" type="button">Adicionar Fornecedor</Button>
+                    </Form>
+                </Card>
+            </Row>
+            <Row>
+                <Col>
+                    <Table responsive striped hover variant="light">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>nome</th>
+                                <th>cnpj</th>
+                                <th>telefone</th>
+                                <th>endereco</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>0</th>
+                                <th>Ambev LTDA</th>
+                                <th>00.000.000/0001-00</th>
+                                <th>(00) 00000-0000</th>
+                                <th>R Algum lugar, 1100 - jardim intregracao - franca-sp</th>
+                            </tr>
+                        </tbody>
+                    </Table>
                 </Col>
             </Row>
         </Container>
