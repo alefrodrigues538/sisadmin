66b860c8deb6a3f510e7efa6412828074982f095
alefrodrigues538 <alefrodrigues538@gmail.com>
2020-11-23 13:06:12 -0300

    v0.01.2

--- frontend/src/pages/Fornecedores/index.js
+++ frontend/src/pages/Fornecedores/index.js
@@ -1,10 +1,18 @@
 import React from "react"
 import "./styles.css"
-import { Container, Row, Col,
-Form, Button, } from "react-bootstrap"
+import {
+    Container, Row, Col,
 
-export default props=>(
-    <Container>
-        <h3>Fornecedores</h3>
-    </Container>
-)
+} from "react-bootstrap"
+
+export default function Fornecedores() {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <h3>Fornecedores</h3>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
