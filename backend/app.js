abb2f79335e02bbbfbf025c467324a040cfbade5
alefrodrigues538 <alefrodrigues538@gmail.com>
2020-11-29 14:20:53 -0300

    v0.01.14

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,13 @@ var corsOptions={
     optionsSuccessStatus:200
 }
 
+//CORS HEADERS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
+});
+
 //PARSERS
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
