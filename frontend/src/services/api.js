c96ce7ba822f791f383f0da816c1746ade75abb9
alefrodrigues538 <alefrodrigues538@gmail.com>
2020-11-29 14:03:09 -0300

    0.01.14

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({ 
-    baseURL: "http://sisadmin.kinghost.net:21008/api",
+    baseURL: "sisadmin.kinghost.net:21008/api",
 });
 
 export default api;
