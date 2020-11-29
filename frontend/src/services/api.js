67930a8f20950ada789d6398162f7d7681c125c2
alefrodrigues538 <alefrodrigues538@gmail.com>
2020-11-29 16:20:20 -0300

    v0.01.19

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({ 
-    baseURL: "http://seuspedidosdelivery.com.br/api",
+    baseURL: "https://seuspedidosdelivery.com.br/api",
 });
 
 export default api;
